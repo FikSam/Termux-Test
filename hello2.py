733d6b19ce71c702f2550205e81d1e78e395101a
FikSam <86853932+FikSam@users.noreply.github.com>
2023-10-16 10:15:49 +0200

    Add files via upload

---
+++ hello2.py
@@ -0,0 +1,12 @@
+
+print("Termux Calculator =•=")
+
+var1 = input("var1: ")
+var2 = input("var2: ")
+operation = input("+, - :")
+if operation == "+":
+  var1 += var2
+  print(var1)
+else:
+  var1 -= var2
+  print(var1)
